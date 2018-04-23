41191f3861f7db3a9ab5740385aa8aca0223f4d3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomColorizer/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraMap;
+
+namespace CustomColorizer {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        const string powerAttrName = "PowerValue";
+        const double maxPower = 1000;
+        const int rectNumber = 50;
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+            // Create a map control, set its dock style and add it to the form.
+            MapControl map = new MapControl();
+            map.Dock = DockStyle.Fill;
+            this.Controls.Add(map);
+
+            // Create a vector items layer and add it to the map.
+            VectorItemsLayer itemsLayer = new VectorItemsLayer();
+            map.Layers.Add(itemsLayer);
+            
+            // Generate map polygons.
+            GenerateVectorItems(itemsLayer.Items);
+
+            // Specify the tooltip content.            
+            itemsLayer.ToolTipPattern = "{" + powerAttrName + "}";
+
+            // Create a custom colorizer.
+            map.Colorizer = new CustomColorizer();
+
+        }
+
+        private void GenerateVectorItems(LayerMapItemCollection col) {
+            int width = 5;
+            double singlePower = maxPower / rectNumber;
+
+            for (int i = 0; i < rectNumber; i++) {
+                MapPolygon polygon = CreatePolygon(i * singlePower,
+                new GeoPoint[] { new GeoPoint(0, width * i), 
+                                 new GeoPoint(0, width * (i + 1)),
+                                 new GeoPoint(40, width * (i + 1)),
+                                 new GeoPoint(40, width * i), 
+                                 new GeoPoint(0, width * i) });
+                col.Add(polygon);
+            }
+        }
+
+
+        private MapPolygon CreatePolygon(double power, GeoPoint[] points) {
+            MapPolygon item = new MapPolygon();
+
+            foreach (GeoPoint point in points) {
+                item.Points.Add(point);
+            }
+
+            item.Attributes.Add(new MapItemAttribute() { Name = powerAttrName, Type = typeof(Double), Value = power });
+
+            return item;
+        }
+
+
+        private class CustomColorizer : MapColorizer {
+
+            Color[] colors = {Color.Violet, Color.Blue, Color.LightBlue, Color.Green,Color.Yellow, Color.Orange, Color.Red};
+
+            public override void ColorizeElement(IColorizerElement element) {
+
+                MapPolygon polygon = element as MapPolygon;
+                if (polygon != null) {
+                    double power = (double)polygon.Attributes[powerAttrName].Value;
+
+                    int linearizedPower = (int)Math.Truncate(power * colors.Length / maxPower);
+
+                    element.ColorizerColor = colors[linearizedPower];
+                }
+            }
+
+        }
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,15 @@
+# How to implement a custom colorizer
+
+
+<p>This example demonstrates how to create a custom colorizer and use it for implementing a progress bar on a map.</p><p>The colorizer fills bars with colors according to the specified power value stored as a shape attribute.</p>
+
+
+<h3>Description</h3>
+
+<p>To implement a custom colorizer, create a class inherited from the <strong>MapColorizer</strong> base class and implement the <strong>ColorizeElement</strong> method according to your custom rules.</p>
+<p>For example, in this case, the custom colorizer does the following:</p>
+<p>1. Obtains the power value from the shape attribute (via the <strong>MapItem.Attributes</strong> property)<br /> 2. Chooses a color that corresponds to this value (e.g. from the predefined array of <strong>Color</strong> objects) <br /> 3. And assigns this color to the <strong>IColorizerElement.ColorizerColor</strong> property of the element passed to the <strong>ColorizeElement</strong> method.</p>
+
+<br/>
+
+
